407212f9b337d0ba6b85fd4be4038cf3c1dcd40b
Pari Mishra <pariimishra07@gmail.com>
2026-07-10 19:23:04 +0530

    Time: 13 ms (84.02%), Space: 20.6 MB (8.93%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goal = len(nums) - 1
-        
-        # Loop backwards from the second-to-last element to the first
+
         for i in range(len(nums) - 2, -1, -1):
             if i + nums[i] >= goal:
                 goal = i
-                
-        # If the goalpost reaches the start, the path exists
-        return goal == 0
+        
+        return True if goal == 0 else False
